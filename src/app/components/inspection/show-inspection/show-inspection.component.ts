13934fbea1080ee69df715e76407dcc23eb51ff6
Andrey <andrey93-15@mail.ru>
2022-02-26 17:32:40 +0200

    Back+Fromt practice

---
+++ src/app/components/inspection/show-inspection/show-inspection.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/services/inspection-api.service';
+
+@Component({
+  selector: 'app-show-inspection',
+  templateUrl: './show-inspection.component.html',
+  styleUrls: ['./show-inspection.component.css']
+})
+export class ShowInspectionComponent implements OnInit {
+
+  inspectionList$!:Observable<any[]>;
+  inspectionTypesList$!:Observable<any[]>;
+  inspectionTypesList:any=[];
+
+  //Map to display data associate with foreigh keys
+  inspectionTypesMap:Map<number, string> = new Map();
+
+  constructor(private service:InspectionApiService) { }
+
+  ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypesList$ = this.service.getInspectionTypesList();
+  }
+
+}
