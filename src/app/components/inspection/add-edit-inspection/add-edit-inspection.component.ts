a69f7e5a5697e4705854c6e812afe57c36897d5a
Andrey <andrey93-15@mail.ru>
2022-02-27 19:43:33 +0200

    Add Inspection form, alerts of success, addInspection()

--- src/app/components/inspection/add-edit-inspection/add-edit-inspection.component.ts
+++ src/app/components/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/services/inspection-api.service';
 
 @Component({
   selector: 'app-add-edit-inspection',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditInspectionComponent implements OnInit {
 
-  constructor() { }
+  inspectionTypesList$!: Observable<any[]>;
+  inspectionList$!: Observable<any[]>;
+  statusList$!:  Observable<any[]>;
+  
+
+  constructor(private service:InspectionApiService) { }
+
+
+// input decorator for sharing data from "father" => "dother"
+//dother
+  @Input() inspection:any;
+  id: number;
+  status: string;
+  comments: string;
+  inspectionTypeId: number;
+
 
   ngOnInit(): void {
+    this.id = this.inspection.id;
+    this.status = this.inspection.status;
+    this.comments = this.inspection.comments;
+    this.inspectionTypeId = this.inspection.inspectionTypeId;
+    this.statusList$ = this.service.getStatusList();
+    this.inspectionList$ = this.service.getInspectionList();
+    this.inspectionTypesList$ = this.service.getInspectionTypesList();
   }
 
+  addInspection(){
+    var inspection = {
+      status: this.status,
+      comments: this.comments,
+      inspectionTypeId: this.inspectionTypeId
+    }
+
+    this.service.addInspection(inspection).subscribe(res =>{
+      
+      var closeModalBtn = document.getElementById('add-edit-modal-close');
+      if(closeModalBtn){
+        closeModalBtn.click();
+      }
+
+      var showAddSuccess = document.getElementById('add-success-alert');
+      if(showAddSuccess){
+        showAddSuccess.style.display="black";
+      }
+
+      setTimeout(function(){
+        if(showAddSuccess){
+          showAddSuccess.style.display = "none";
+        }
+      }, 4000);
+    })
+  }
+
+  updateInspection(){
+
+  }
 }
